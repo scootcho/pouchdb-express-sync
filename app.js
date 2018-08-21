1b4e2ac5fb7906ef5385790dcf4a6265ff74aa26
Scott Yu <scott.tj.yu@gmail.com>
2018-08-21 16:41:33 +0800

    got client and server db to sync

---
+++ app.js
@@ -0,0 +1,18 @@
+var PouchDB = require('pouchdb');
+var cors = require('cors')
+var express = require('express');
+var app = express();
+app.use(cors());
+var PORT = 3000
+
+app.use('/db', require('express-pouchdb')(PouchDB));
+var testDb = new PouchDB('test_db');
+// localhost:3000/db/test_db (available via domain/db/foo)
+// testDb is now modifiable in your own code
+
+var listener = app.listen(PORT, function () {
+  testDb.info().then(function(info) {
+    console.log(info);
+  });
+  console.log('Your app is listening on port ' + listener.address().port);
+});
---
+++ index.js
@@ -0,0 +1,27 @@
+var db = new PouchDB('test_db');
+
+db.post({ "test": 123 }).then(function(result) {
+  console.log('======== finish writing to local db ======', result);
+}, function(error) {
+  console.log(error);
+});
+
+var remoteDB = new PouchDB('http://localhost:3000/db/test_db')
+// check http://localhost:3000/db/test_db to see if server db is updated per client db
+
+db.sync(remoteDB).on('complete', function () {
+  // yay, we're in sync!
+  console.log('===================== sync completed!!! ====================');
+}).on('error', function (err) {
+  // boo, we hit an error!
+  console.log('===================== sync error!!! ====================', err);
+});
+
+db.allDocs({include_docs: true, descending: true})
+  .then(function(results){
+    console.log('============= Query allDocs ==============', results)
+  })
+  .catch(function(error) {
+    console.log('=========== error ===========', error)
+})
+
